43c02285975b786837aa696925e9f867c5953b22
libiao <578556500@qq.com>
2020-12-11 15:27:23 +0800

    firsr commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### 动态路由
+在 vuex中获取 route 表
+在 router.js中配置通用路由
+在 permission 中 通过 addRouter 添加动态路由 
+可不用 管 permission 中的 amdin 判断  直接获取动态路由 加上 通用路由组合
---
+++ src/permission.js
@@ -0,0 +1,70 @@
+import router, { adminRoutes, endRoutes } from './router'
+import store from './store'
+import { Message } from 'element-ui'
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+import { getToken } from '@/utils/auth' // get token from cookie
+import getPageTitle from '@/utils/get-page-title'
+
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
+
+const whiteList = ['/login'] // no redirect whitelist
+
+router.beforeEach(async (to, from, next) => {
+  // start progress bar
+  NProgress.start()
+
+  // set page title
+  document.title = getPageTitle(to.meta.title)
+
+  // determine whether the user has logged in
+  const hasToken = getToken()
+
+  if (hasToken) {
+    if (to.path === '/login') {
+      // if is logged in, redirect to the home page
+      next({ path: '/' })
+      NProgress.done()
+    } else {
+      const hasGetUserInfo = store.getters.name
+      if (hasGetUserInfo) {
+        next()
+      } else {
+        try {
+          // get user info
+          await store.dispatch('user/getInfo')
+          const userRole = store.getters.role  // 获取用户权限
+          console.log(router.options.routes)
+          // 简写  判断当前用户是否是管理员 如果是则添加管理员路由 不是就只添加末尾路由
+          const addRoutes = userRole === 'admin' ? [...adminRoutes, ...endRoutes] : [...endRoutes]
+          router.addRoutes(addRoutes)
+         
+          router.options.routes = router.options.routes.concat(addRoutes)
+          console.log('添加addRoutes 后', router.options.routes)
+          next(to.fullPath)
+        } catch (error) {
+          // remove token and go to login page to re-login
+          await store.dispatch('user/resetToken')
+          Message.error(error || 'Has Error')
+          next(`/login?redirect=${to.path}`)
+          NProgress.done()
+        }
+      }
+    }
+  } else {
+    /* has no token*/
+    if (whiteList.indexOf(to.path) !== -1) {
+      // in the free login whitelist, go directly
+      next()
+    } else {
+      // other pages that do not have permission to access are redirected to the login page.
+      next(`/login?redirect=${to.path}`)
+      NProgress.done()
+    }
+  }
+})
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
+})
